014c135e88f0b9950e515bb6d64e86175a7ae371
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2021-05-22 23:41:17 +0200

    fixing module

---
+++ index.js
@@ -0,0 +1,10 @@
+import {DirTreeProvider, useDirTree} from './src/context/DirTreeProvider';
+import Directory from './src/Directory';
+import LazyDirectory from './src/LazyDirectory'
+
+export const {
+    DirTreeProvider,
+    useDirTree,
+    Directory,
+    LazyDirectory
+}
